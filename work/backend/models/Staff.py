fa813c941293689e8ac5fa938a21ef9d66cb8a06
Degala Aman <degalaaman0@gmail.com>
2024-07-09 12:40:45 +0530

    added password field to staff

--- work/backend/models/Staff.py
+++ work/backend/models/Staff.py
@@ -9,16 +9,19 @@ class Staff(db.Model):
     s_email=db.Column(db.String(),nullable=False)
     s_isAdmin=db.Column(db.Boolean,nullable=False)
     s_contact=db.Column(db.Integer,nullable=False)
+    s_password=db.Column(db.String(),nullable=False)
 
     def __init__(self,data):
         s_name=data["s_name"]
         s_email=data["s_email"]
         s_isAdmin=data["s_isAdmin"]
         s_contact=data["s_contact"]
+        s_password=data["s_password"]
         self.s_name = s_name      
         self.s_email = s_email     
         self.s_isAdmin=s_isAdmin   
         self.s_contact = s_contact
+        self.s_password=s_password
     
     def register_user_if_not_exist(self):        
         db_staff = Staff.query.filter(Staff.s_contact == self.s_contact).all()
